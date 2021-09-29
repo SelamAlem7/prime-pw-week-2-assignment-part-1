ac60b85634f1fb5191bf624390d5ab20626a6a9d
Selam Alem <selamtalem@gmail.com>
2021-09-28 20:06:39 -0500

    finished adding first few variables

---
+++ variablesPractical/variables.js
@@ -0,0 +1,32 @@
+console.log('js');
+
+
+let testVar = 9;
+
+console.log(testVar);
+
+console.log('the value of testVar is', testVar);
+
+testVar = 7197495;
+
+console.log('the value of testVar is', testVar);
+
+testVar = 'some other text';
+
+
+let MyAge = 29;
+
+console.log('this is how old i am', MyAge);
+
+let BirthPlace = 'Ethiopia';
+
+console.log('this is where i was born', BirthPlace);
+
+let breakTime = '2:30pm' ;
+
+console.log('the time i took break was', breakTime);
+
+
+let sibilings = 3;
+let pets = 2;
+console.log('family members', sibilings + pets);
