09208d4e250c10ec848bf7020e068bb23767ea05
Selam Alem <selamtalem@gmail.com>
2021-09-30 15:42:03 -0500

    I completed a compound conditional

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -20,23 +20,40 @@ console.log( luckyNumber );
 console.log('My name is', fullName + ' and I think', luckyNumber+ ' is a winner!');
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
+let adventurous = false;
 
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
+let food = 'pasta';
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
+let myPets = 1;
 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
+let friendsPets = 2;
 
 // 11 - Add two pets to your `pets` variable
+myPets += 2;
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
+const allowedPets = 2;
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
-
+if( adventurous) {
+  console.log( 'Adventures are great! ');
+}
+else{
+  console.log( 'How about we stay home?');
+}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
+if( luckyNumber === 2) {
+  console.log( 'Roll the dice!');
+}
+if( adventurous === true) {
+  console.log( 'Roll the dice!');
+}
 
 // 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
--- variablesPractical/variables.js
+++ variablesPractical/variables.js
@@ -20,13 +20,63 @@ console.log('this is how old i am', MyAge);
 
 let BirthPlace = 'Ethiopia';
 
-console.log('this is where i was born', BirthPlace);
+console.log('this is where i was born:', BirthPlace);
 
 let breakTime = '2:30pm' ;
 
-console.log('the time i took break was', breakTime);
+console.log('the time i took break was:', breakTime);
 
 
 let sibilings = 3;
 let pets = 2;
 console.log('family members', sibilings + pets);
+
+let motorcycles = 9;
+
+console.log('bikes', motorcycles );
+console.log('had to put the soft tail in the garage');
+
+//decrement motorcycles
+motorcycles--; //this will subtract 1 from the value of mororcycles
+console.log('bikes', motorcycles );
+
+
+let s = 50
+let c = 100
+
+console.log('The total of my bank account is', s + c);
+
+s = 20;
+c = 50;
+console.log('I went shopping', s + c);
+
+s= 200;
+c = 200;
+console.log('I got my paycheck from my job', s + c);
+
+
+let olderSister = 'rekike';
+let middleSister = 'lidiya';
+console.log('These are my sibilings:', olderSister + ' and', middleSister );
+
+
+let seatsInCar = 7;
+let passangers = 5;
+let infants = 1;
+let infantSeats = 1;
+let fullTank = true;
+
+
+if ( seatsInCar >= passangers && infantSeats >= infants ){
+  console.log( 'ready for road trip' );
+  if ( fullTank === true ){
+    console.log( 'ready to hit the highway' );
+  } //end fullTank
+  else {
+    console.log( 'must gas up before on interstate ');
+  }
+} //end with enough seats
+
+else{
+  console.log( 'not ready, check car, passangers, and equipment');
+} //end eith not enough seats
